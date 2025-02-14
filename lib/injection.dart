4c45fd07aba646bcb13af4d8167309fd068f9da8
Trevor Miller <tcmiller.dev@proton.me>
2025-02-14 13:29:25 +1000

    Add note repository

---
+++ lib/injection.dart
@@ -0,0 +1,25 @@
+// This source code is copyright ot Trevor Miller.
+// Trevor Miller licenses this file to You under the MIT License (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://github.com/trevorcharlesmiller/basic-notes/blob/main/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'injection.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  initializerName: 'init', // default
+  preferRelativeImports: true, // default
+  asExtension: true, // default
+)
+void configureDependencies() => getIt.init();
