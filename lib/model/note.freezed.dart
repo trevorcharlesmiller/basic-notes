32dd7ac65ae15de43ac5ff3fbcbcb220f76a758d
Trevor Miller <tcmiller.dev@proton.me>
2025-02-14 11:12:20 +1000

    add model classes

---
+++ lib/model/note.freezed.dart
@@ -0,0 +1,187 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'note.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Note _$NoteFromJson(Map<String, dynamic> json) {
+  return _Note.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Note {
+  String get id => throw _privateConstructorUsedError;
+  String get abstract => throw _privateConstructorUsedError;
+
+  /// Serializes this Note to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Note
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $NoteCopyWith<Note> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NoteCopyWith<$Res> {
+  factory $NoteCopyWith(Note value, $Res Function(Note) then) =
+      _$NoteCopyWithImpl<$Res, Note>;
+  @useResult
+  $Res call({String id, String abstract});
+}
+
+/// @nodoc
+class _$NoteCopyWithImpl<$Res, $Val extends Note>
+    implements $NoteCopyWith<$Res> {
+  _$NoteCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Note
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? abstract = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      abstract: null == abstract
+          ? _value.abstract
+          : abstract // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$NoteImplCopyWith<$Res> implements $NoteCopyWith<$Res> {
+  factory _$$NoteImplCopyWith(
+          _$NoteImpl value, $Res Function(_$NoteImpl) then) =
+      __$$NoteImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String id, String abstract});
+}
+
+/// @nodoc
+class __$$NoteImplCopyWithImpl<$Res>
+    extends _$NoteCopyWithImpl<$Res, _$NoteImpl>
+    implements _$$NoteImplCopyWith<$Res> {
+  __$$NoteImplCopyWithImpl(_$NoteImpl _value, $Res Function(_$NoteImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Note
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? abstract = null,
+  }) {
+    return _then(_$NoteImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      abstract: null == abstract
+          ? _value.abstract
+          : abstract // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$NoteImpl with DiagnosticableTreeMixin implements _Note {
+  const _$NoteImpl({required this.id, this.abstract = ''});
+
+  factory _$NoteImpl.fromJson(Map<String, dynamic> json) =>
+      _$$NoteImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  @JsonKey()
+  final String abstract;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Note(id: $id, abstract: $abstract)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Note'))
+      ..add(DiagnosticsProperty('id', id))
+      ..add(DiagnosticsProperty('abstract', abstract));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$NoteImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.abstract, abstract) ||
+                other.abstract == abstract));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, abstract);
+
+  /// Create a copy of Note
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$NoteImplCopyWith<_$NoteImpl> get copyWith =>
+      __$$NoteImplCopyWithImpl<_$NoteImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$NoteImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Note implements Note {
+  const factory _Note({required final String id, final String abstract}) =
+      _$NoteImpl;
+
+  factory _Note.fromJson(Map<String, dynamic> json) = _$NoteImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get abstract;
+
+  /// Create a copy of Note
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$NoteImplCopyWith<_$NoteImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/model/note_list.freezed.dart
@@ -0,0 +1,173 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'note_list.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+NoteList _$NoteListFromJson(Map<String, dynamic> json) {
+  return _NoteList.fromJson(json);
+}
+
+/// @nodoc
+mixin _$NoteList {
+  List<Note> get notes => throw _privateConstructorUsedError;
+
+  /// Serializes this NoteList to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of NoteList
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $NoteListCopyWith<NoteList> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NoteListCopyWith<$Res> {
+  factory $NoteListCopyWith(NoteList value, $Res Function(NoteList) then) =
+      _$NoteListCopyWithImpl<$Res, NoteList>;
+  @useResult
+  $Res call({List<Note> notes});
+}
+
+/// @nodoc
+class _$NoteListCopyWithImpl<$Res, $Val extends NoteList>
+    implements $NoteListCopyWith<$Res> {
+  _$NoteListCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of NoteList
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? notes = null,
+  }) {
+    return _then(_value.copyWith(
+      notes: null == notes
+          ? _value.notes
+          : notes // ignore: cast_nullable_to_non_nullable
+              as List<Note>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$NoteListImplCopyWith<$Res>
+    implements $NoteListCopyWith<$Res> {
+  factory _$$NoteListImplCopyWith(
+          _$NoteListImpl value, $Res Function(_$NoteListImpl) then) =
+      __$$NoteListImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({List<Note> notes});
+}
+
+/// @nodoc
+class __$$NoteListImplCopyWithImpl<$Res>
+    extends _$NoteListCopyWithImpl<$Res, _$NoteListImpl>
+    implements _$$NoteListImplCopyWith<$Res> {
+  __$$NoteListImplCopyWithImpl(
+      _$NoteListImpl _value, $Res Function(_$NoteListImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of NoteList
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? notes = null,
+  }) {
+    return _then(_$NoteListImpl(
+      notes: null == notes
+          ? _value.notes
+          : notes // ignore: cast_nullable_to_non_nullable
+              as List<Note>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$NoteListImpl with DiagnosticableTreeMixin implements _NoteList {
+  const _$NoteListImpl({this.notes = const []});
+
+  factory _$NoteListImpl.fromJson(Map<String, dynamic> json) =>
+      _$$NoteListImplFromJson(json);
+
+  @override
+  @JsonKey()
+  final List<Note> notes;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'NoteList(notes: $notes)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'NoteList'))
+      ..add(DiagnosticsProperty('notes', notes));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$NoteListImpl &&
+            const DeepCollectionEquality().equals(other.notes, notes));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(notes));
+
+  /// Create a copy of NoteList
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$NoteListImplCopyWith<_$NoteListImpl> get copyWith =>
+      __$$NoteListImplCopyWithImpl<_$NoteListImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$NoteListImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _NoteList implements NoteList {
+  const factory _NoteList({final List<Note> notes}) = _$NoteListImpl;
+
+  factory _NoteList.fromJson(Map<String, dynamic> json) =
+      _$NoteListImpl.fromJson;
+
+  @override
+  List<Note> get notes;
+
+  /// Create a copy of NoteList
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$NoteListImplCopyWith<_$NoteListImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
